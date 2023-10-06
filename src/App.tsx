7fa7e0b5a1da248abc6d380983b079bb6092947d
kaveeshanethruwan <kaveesha.james151@gmail.com>
2023-10-06 21:18:00 +0530

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Outlet, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>Getting started</div>
+      <div id="sidebar">
+        <nav>
+          <ul>
+            <li>
+              <Link to={`stopwatch`}>Stopwatch</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail"></div>
+
+      <div id="detail">
+        <Outlet />
+      </div>
     </>
   );
 }
